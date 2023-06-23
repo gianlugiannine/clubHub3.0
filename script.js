2f4f1aa89be4b96ba0fc9d15f470ef8abd782f6c
gianlugiannine <ggianninelizarraga@itba.edu.ar>
2023-06-23 05:04:35 -0300

    confirmacionEdad

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+
+var mensaje1 = "Hola";
+var mensaje2 = " Mundo";
+var mensaje = mensaje1 + mensaje2; 
+
+document.write(mensaje);
+
+
+function checkAge(age) {
+    if (age >= 18) {
+      return true;
+    } 
+    else {
+      return confirm('Acceso denegado. Su edad debe ser 18');
+    }
+
+  }
+  
+  let age = prompt('Ingresa tu edad a continuacion', 18);
+  
+  if ( checkAge(age) ) {
+    alert( 'Acceso permitido, Disfruta de la fiesta!' );
+  } 
+  else {
+    alert( 'Acceso denegado. Su edad debe ser 18' );
+  }
+
+
+
+
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
